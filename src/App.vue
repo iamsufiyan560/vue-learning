a13ea1be3442f627bf0e75cf0f8c36dfd8f7fc64
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-07-12 20:42:06 +0530

    slots & provide & inject

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,63 @@
 <script setup>
-import EventValidation from "./ChildToParent/EventValidation.vue";
-import { ref } from "vue";
+import { provide } from "vue";
+import Page from "./Provide_And_Inject/Page.vue";
+
+// Using Provide With Array
+provide("friends", ["Alex", "Jordan", "HuXn", "John"]);
+
+// Using Provide With Object
+provide("games", {
+  id: 1,
+  title: "Epic Adventure Game",
+  genre: ["Action", "Adventure", "RPG"],
+  platform: ["PC", "PlayStation", "Xbox", "Nintendo Switch"],
+  releaseDate: "2022-03-15",
+  developer: {
+    name: "Awesome Game Studios",
+    location: "San Francisco, CA",
+    teamSize: 50,
+  },
+});
+
+// Using Provide With Array Of Objects
+provide("moreGames", [
+  {
+    title: "Space Explorer",
+    genre: "Sci-Fi",
+    platform: "PC",
+    releaseDate: "2023-05-20",
+  },
+  {
+    title: "Fantasy Quest",
+    genre: "Fantasy",
+    platform: "PlayStation",
+    releaseDate: "2023-07-10",
+  },
+  {
+    title: "Inception",
+    director: "Christopher Nolan",
+    genre: "Sci-Fi",
+    releaseDate: "2010-07-16",
+  },
+  {
+    title: "The Shawshank Redemption",
+    director: "Frank Darabont",
+    genre: "Drama",
+    releaseDate: "1994-09-23",
+  },
+]);
+
+// import Students from "./Provide_And_Inject/Students.vue";
+
+// provide("studentName", "ABC");
+// provide("studentAge", 20);
+// provide("studentLocation", ["india", "delhi"]);
+
+// import EventValidation from "./ChildToParent/EventValidation.vue";
+// import { ref } from "vue";
+// import SlotComponent from "./Slots/SlotComponent.vue";
+// import FallbackContent from "./Slots/FallbackContent.vue";
+// import NamedSlot from "./Slots/NamedSlot.vue";
 
 // let count = ref(0);
 // const increment = () => count.value++;
@@ -54,7 +111,30 @@ import { ref } from "vue";
   <!-- <ComponentEvent @incrementCount="count++" /> -->
   <!-- <ComponentEvent @incrementCount="increment" /> -->
   <!-- <EventValidation @userInfo="formHandler" /> -->
-  <div></div>
+
+  <div>
+    <!-- <SlotComponent>
+      <h1>Content 1</h1>
+      <h2>Content 2</h2>
+    </SlotComponent>
+    <hr />
+    <FallbackContent /> -->
+  </div>
+
+  <!-- <NamedSlot>
+    <template v-slot:one>
+      <h1>This content will go to slot "one"</h1>
+    </template> -->
+
+  <!-- Alternative way -->
+  <!-- <template #two>
+      <h1>This content will go to slot "two"</h1>
+    </template>
+  </NamedSlot> -->
+
+  <!-- <Students /> -->
+
+  <Page></Page>
 </template>
 
 <style scoped></style>
